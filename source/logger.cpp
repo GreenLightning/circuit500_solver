6a202a7920ee5645e1f5b1a14f715341b1ad6781
Green Lightning <GreenLightning.git@googlemail.com>
2015-12-05 16:22:16 +0100

    Update log format.

--- source/logger.cpp
+++ source/logger.cpp
@@ -10,31 +10,42 @@
 
 namespace pt = boost::posix_time;
 
-Logger::Logger() {
+Logger::Logger() : level_count(0), total_solutions(0), total_time(std::chrono::nanoseconds(0)) {
 	std::ostringstream log_name;
 	log_name.imbue(std::locale(log_name.getloc(), new pt::time_facet("%Y-%m-%d_%H.%M.%S")));
 	log_name << "data/logs/log_" << pt::microsec_clock::local_time() << ".txt";
-	log_file = std::ofstream(log_name.str());
-	log_file << "level; solutions; nanoseconds" << std::endl;
-	count = 0;
-	current_level = 0;
+	this->log_file = std::ofstream(log_name.str());
+	this->log_file << "level; solutions; nanoseconds" << std::endl;
+	this->current_level = 0;
+}
+
+Logger::~Logger() {
+	log_file << "---------- totals -----------" << std::endl;
+	log_file << std::setw(5) << level_count << ";";
+	log_file << std::setw(10) << total_solutions << ";";
+	log_file << std::setw(12) << total_time.count()  << "\n";
 }
 
 long long int & Logger::start_search(int level) {
-	if (current_level != 0)
-		throw std::runtime_error("search already started");
 	current_level = level;
 	solutions_checked = 0;
 	start = std::chrono::high_resolution_clock::now();
+
 	return solutions_checked;
 }
 
 void Logger::stop_search() {
-	if (current_level == 0)
-		throw std::runtime_error("search not started");
 	std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-	log_file << std::setw(5) << current_level << ";" << std::setw(10) << solutions_checked << ";" << std::setw(12) << duration  << "\n";
-	if (++count % 10 == 0) log_file.flush();
+	std::chrono::nanoseconds duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+	level_count += 1;
+	total_solutions += solutions_checked;
+	total_time += duration;
+	
+	log_file << std::setw(5) << current_level << ";";
+	log_file << std::setw(10) << solutions_checked << ";";
+	log_file << std::setw(12) << duration.count()  << "\n";
+	if (level_count % 10 == 0) log_file.flush();
+
 	current_level = 0;
 }
--- source/logger.hpp
+++ source/logger.hpp
@@ -7,11 +7,16 @@
 class Logger {
 public:
 	Logger();
+	~Logger();
 	long long int &start_search(int level);
 	void stop_search();
 private:
 	std::ofstream log_file;
-	int count;
+	
+	int level_count;
+	long long int total_solutions;
+	std::chrono::nanoseconds total_time;
+
 	int current_level;
 	long long int solutions_checked;
 	std::chrono::high_resolution_clock::time_point start;
